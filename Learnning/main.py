3744d4c583ff914b654a42ef7f6af3c027b86ef6
Edotnd <edotnd@gmail.com>
2020-09-10 23:52:44 +0900

    starting BlockChain

---
+++ Learnning/main.py
@@ -0,0 +1,11 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # 새로운 블록을 만들고 체인 연결
+        pass
+
+    def new_transaction(self):
+        # 
